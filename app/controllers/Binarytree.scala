2d4769a82e2a010dfb6bdb4feedfb3b1fde6f602
Andrew Sverdrup <sverdra1@my.erau.edu>
2017-04-16 22:43:32 -0700

    Implemented binary tree

--- app/controllers/Binarytree.scala
+++ app/controllers/Binarytree.scala
@@ -9,7 +9,15 @@ class Binarytree @Inject() extends Controller {
 
    def insert = Action { implicit request =>
      request.body.asFormUrlEncoded.get("action").headOption match {
-       case Some("insert") => Ok("Clicked insert " + binaryTree.getx())
+       case Some("insert") => {
+          // extract item from DOM to use for whatever operation was pressed
+          //http://stackoverflow.com/questions/11212248/how-do-i-access-post-data-from-scala-play
+          val userNum = request.body.asFormUrlEncoded.get("userNum").lift(0)
+          userNum map {_.toInt} getOrElse 0
+
+          Ok("Clicked insert " + binaryTree.insert(userNum.get.toInt))
+       }
+
        case Some("remove") => Ok("Clicked remove")
        case _ => BadRequest("This action is not allowed")
      }
--- app/controllers/binaryTreeClass.scala
+++ app/controllers/binaryTreeClass.scala
@@ -1,27 +1,97 @@
 package binarytreepkg
 
-class node {
+class Node(inData:Int) {
+   var data : Int = inData;
+   var rightChild : Node = _;
+   var leftChild : Node = _;
 
 }
 
 class binaryTreeClass {
-   val x = 5;
 
-   val root = null;
+   // syntax - update
+   // http://stackoverflow.com/questions/11215915/scala-type-constraints-doesnt-allow-null
+   var root : Node = _;
 
-   def preOrderTraversal(startNode:node): String = {
-      "pre"
+   var traversalResult : String = "";
+
+   var result : String = "";
+
+   def insert(data:Int) = {
+      // Insertion of first node
+      val newNode = new Node(data)
+
+      var trvPtr = root
+
+      if (root == null)
+         root = newNode
+      else { // Insertion of additional nodes
+         while (trvPtr != null) {
+            val trvPtrParent = trvPtr
+            if (newNode.data < trvPtr.data) {
+               trvPtr = trvPtr.leftChild
+               if (trvPtr == null) {
+                  trvPtrParent.leftChild = newNode
+               }
+            } else {
+               trvPtr = trvPtr.rightChild
+               if (trvPtr == null) {
+                  trvPtrParent.rightChild = newNode
+               }
+            }
+         } // end while
+      } // end if/else
+      result = data.toString + " inserted!"
    }
-   def inOrderTraversal(startNode:node): String = {
-      "in"
+
+
+
+   def visit(nodeToVisit : Node) = { /* Pure function */
+      this.traversalResult + nodeToVisit.data
    }
-   def postOrderTraversal(startNode:node): String = {
-      "post"
+
+   def preOrderTraversal(startNode : Node): String = {
+
+      if (startNode == this.root) // reset result variable to print traversals
+         this.traversalResult = ""
+
+      if (startNode != null) {
+         traversalResult = visit(startNode)
+         preOrderTraversal(startNode.leftChild)
+         preOrderTraversal(startNode.rightChild)
+      }
+
+      this.traversalResult
+
+   }
+   def inOrderTraversal(startNode : Node): String = {
+      if (startNode == this.root) // reset result variable to print traversals
+         this.traversalResult = ""
+
+      if (startNode != null) {
+         inOrderTraversal(startNode.leftChild)
+         traversalResult = visit(startNode)
+         inOrderTraversal(startNode.rightChild)
+      }
+
+      this.traversalResult
+   }
+   def postOrderTraversal(startNode : Node): String = {
+      if (startNode == this.root) // reset result variable to print traversals
+         this.traversalResult = ""
+
+      if (startNode != null) {
+         postOrderTraversal(startNode.leftChild)
+         postOrderTraversal(startNode.rightChild)
+         traversalResult = visit(startNode)
+      }
+
+      this.traversalResult
    }
 }
 
 object binaryTree extends binaryTreeClass {
-   def getx(): Int = {
-      x
+   def getVal(): Int = {
+      99
    }
 }
